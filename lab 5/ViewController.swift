50b0a23800b697586f6d58b5e072c05533153902
Max Moede <max.j.moede@gmail.com>
2018-01-29 10:01:43 -0800

    Initial Commit

---
+++ lab 5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab 5
+//
+//  Created by Max Moede on 1/29/18.
+//  Copyright © 2018 Max Moede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
